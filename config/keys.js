a79adbdd8871164bc38405fca63060f30f70f98e
Sanjay <sodhasanjay14@gmail.com>
2019-09-11 19:25:09 +0530

    APis created

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // mongoURI : 'mongodb://localhost:270137/devconnector'
-    mongoURI : 'mongodb+srv://Sanjay:Sanjay14@cluster0-kuvls.mongodb.net/devconnector?retryWrites=true&w=majority'
-}
+  mongoURI: 'mongodb+srv://Sanjay:Sanjay14@cluster0-kuvls.mongodb.net/devconnector?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require('./routes/api/user');
+const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
 mongoose
-    .connect(db)
-        .then(()=>{console.log("::DB Connected ::")})
-    .catch((err)=>{console.log("!!DB Disonnected !!",err)})
+  .connect(db)
+  .then(()=>{console.log("::DB Connected ::")})
+  .catch((err)=>{console.log("!!DB Disonnected !!",err)})
 
-app.use('/api/users',users);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts);
+// Passport middleware
+app.use(passport.initialize());
 
-const port = process.env.port || 5000;
-app.listen(port,()=>{
-    console.log(`Server Running : ${port}`);
-});
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`:::Server Running : ${port}`));
